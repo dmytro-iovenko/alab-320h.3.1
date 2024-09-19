5712ea2f76a1930f4ace0fbea2446dac929bd43a
Dmytro Iovenko <iovenko@gmail.com>
2024-09-19 16:21:51 -0400

    add Learner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { useState } from "react";
+import learnersData from "./data/learners";
+import Learner from "./components/Learner";
 
 function App() {
-
+  const [learners, setLearners] = useState(learnersData);
   return (
     <>
-      
+      {learners.map((learner) => (
+        <Learner key={learner.name} learner={learner} />
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Learner.jsx
@@ -0,0 +1,8 @@
+export default function Learner({ learner }) {
+  return (
+    <div>
+      <h2>{learner.name}</h2>
+      <p>{learner.bio}</p>
+    </div>
+  );
+}
